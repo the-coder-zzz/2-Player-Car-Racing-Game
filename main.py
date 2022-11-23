c875a48b0dacf501a81bcc68c904554444242460
KateJosephine <113868156+KateJosephine@users.noreply.github.com>
2022-11-23 13:50:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
 TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
 
 TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
-RACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
+TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
 
 FINISH = pygame.image.load("imgs/finish.png")
 FINISH_MASK = pygame.mask.from_surface(FINISH)
 FINISH_POSITION = (130, 250)
 
-RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.45)
-GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"), 0.45)
+RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
+GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"), 0.55)
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -25,8 +25,9 @@ PATH = [(175, 119), (110, 70), (56, 133), (70, 481), (318, 731), (404, 680), (41
         (734, 399), (611, 357), (409, 343), (433, 257), (697, 258), (738, 123), (581, 71), (303, 78), (275, 377), (176, 388), (178, 260)]
 
 
+# Rotating the Car
+# Parent Class
 class AbstractCar:
-
     def __init__(self, max_vel, rotation_vel):
         self.img = self.IMG
         self.max_vel = max_vel
@@ -48,9 +49,9 @@ class AbstractCar:
     def move_forward(self):
         self.vel = min(self.vel + self.acceleration, self.max_vel)
         self.move()
-        
+
     def move_backward(self):
-        self.vel = max(self.vel - self.acceleration, -self.max_vel / 2)
+        self.vel = max(self.vel - self.acceleration, -self.max_vel/2)
         self.move()
 
     def move(self):
@@ -60,7 +61,7 @@ class AbstractCar:
 
         self.y -= vertical
         self.x -= horizontal
-        
+
     def collide(self, mask, x=0, y=0):
         car_mask = pygame.mask.from_surface(self.img)
         offset = (int(self.x - x), int(self.y - y))
@@ -71,12 +72,11 @@ class AbstractCar:
         self.x, self.y = self.START_POS
         self.angle = 0
         self.vel = 0
-     
-    
+
 class PlayerCar(AbstractCar):
     IMG = RED_CAR
     START_POS = (180, 200)
-    
+
     def reduce_speed(self):
         self.vel = max(self.vel - self.acceleration / 2, 0)
         self.move()
@@ -84,13 +84,13 @@ class PlayerCar(AbstractCar):
     def bounce(self):
         self.vel = -self.vel
         self.move()
-        
-        
+
+
 class ComputerCar(AbstractCar):
     IMG = GREEN_CAR
     START_POS = (150, 200)
 
-    def __init__(self, max_vel, rotation_vel, path=[]):
+    def __init__(self, max_vel, rotation_vel, path = []):
         super().__init__(max_vel, rotation_vel)
         self.path = path
         self.current_point = 0
@@ -140,7 +140,8 @@ class ComputerCar(AbstractCar):
         self.calculate_angle()
         self.update_path_point()
         super().move()
-        
+
+
 def draw(win, images, player_car, computer_car):
     for img, pos in images:
         win.blit(img, pos)
@@ -149,7 +150,7 @@ def draw(win, images, player_car, computer_car):
     computer_car.draw(win)
     pygame.display.update()
 
-    
+
 def move_player(player_car):
     keys = pygame.key.get_pressed()
     moved = False
@@ -168,11 +169,12 @@ def move_player(player_car):
     if not moved:
         player_car.reduce_speed()
 
+
 def handle_collision(player_car, computer_car):
     if player_car.collide(TRACK_BORDER_MASK) != None:
         player_car.bounce()
 
-   computer_finish_poi_collide = computer_car.collide(
+    computer_finish_poi_collide = computer_car.collide(
         FINISH_MASK, *FINISH_POSITION)
     if computer_finish_poi_collide != None:
         player_car.reset()
